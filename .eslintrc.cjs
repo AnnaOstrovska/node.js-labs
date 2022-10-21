907c333e032e252475e97e7cddd1473d18975a6d
Anna Ostrovska <aostrovska@star.global>
2022-10-21 20:23:29 +0300

    fix code style with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 module.exports = {
-    env: {
-        node: true,
-        es2021: true,
-    },
-    extends: [
-        'standard',
-        'plugin:sonar/recommended',
-        'plugin:n/recommended',
-        'eslint-config-prettier',
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['sonar', 'n'],
-    globals: {
-        Response: 'readonly',
-    },
-    rules: {},
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'standard',
+    'plugin:sonar/recommended',
+    'plugin:n/recommended',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['sonar', 'n'],
+  globals: {
+    Response: 'readonly',
+  },
+  rules: {},
 }
